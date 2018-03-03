e2376bb384edaf33e603f94ba2cf52d09e048d6b
Sarah Depew <sdepew@brynmawr.edu>
2018-03-02 19:38:21 -0500

    finished task 2

---
+++ src/hash.java
@@ -0,0 +1,15 @@
+public class hash {
+    public static void main(String args[]) {
+        String s = "Bryn Mawr";
+        System.out.println(s.hashCode());
+
+        s = "K-Cass";
+        System.out.println(s.hashCode());
+
+        s = "Haverford";
+        System.out.println(s.hashCode());
+
+        s = "Swarthmore";
+        System.out.println(s.hashCode());
+    }
+}
